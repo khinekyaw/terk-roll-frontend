4a2e5828c9d470d5d54627bf9e67b15876fb6a00
daniel <danielrio126@gmail.com>
2025-04-19 23:42:12 +0700

    feat: adaptive logo colors

--- components/icons/abstract-logo.tsx
+++ components/icons/abstract-logo.tsx
@@ -15,7 +15,7 @@ export const AbstractLogo = (props: IProps) => {
       <g strokeLinejoin="bevel" strokeWidth="8">
         <path stroke="#612CFE" d="M61 11.298v91.404H11V59.798h32v24.716H26.5" />
         <path stroke="#2CA6E7" d="M54 5.298v91.404H4V53.798h32v24.716H19.5" />
-        <path stroke="#000" d="M57 7.163v91.404H7V55.664h32V80.38H22.5" />
+        <path className="stroke-foreground" d="M57 7.163v91.404H7V55.664h32V80.38H22.5" />
       </g>
     </svg>
   );
